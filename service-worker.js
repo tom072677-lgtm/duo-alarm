9d6039d855807ca6bc5449f6a349b1f583968836
tom072677-lgtm <cys2727@snu.ac.kr>
2026-05-04 01:47:28 +0900

    service-worker.js 보완

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,22 @@
-self.addEventListener('install', function(e) {
-    console.log('Service Worker 설치됨');
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open('duo-alarm-v1').then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icon-192.jpg',
+                './icon-512.jpg'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('Service Worker 활성화됨');
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', function(e) {
-    e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
 });
